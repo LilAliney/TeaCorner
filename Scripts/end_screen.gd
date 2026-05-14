c4c415807f01cc53e6969004a279d911c30f2710
LilAliney <aline.yoshinari@gmail.com>
2026-05-15 00:07:14 +0200

    Fix: Coin counter

--- Scripts/end_screen.gd
+++ Scripts/end_screen.gd
@@ -1,12 +1,10 @@
 extends CanvasLayer
-# Voeg toe aan groep "end_screen" in de Inspector!
-# Zet process_mode op "Always" zodat het werkt als game gepauzeerd is
 
 func _ready():
 	hide()
 
 func reveal():
-	$Panel/VBoxContainer/CoinsLabel.text = "Je verdiende 🪙 %d munten!" % Economy.coins
+	$Panel/VBoxContainer/CoinsLabel.text = "Je hebt 🪙 %d munten verdiend!\nJe café is een succes!" % Economy.coins
 	get_tree().paused = true
 	visible = true
 
